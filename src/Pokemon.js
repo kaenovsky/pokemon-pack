792ffc3ce2bcd7381f202b2230b777bea8a48136
kaenovsky <martinkaen@gmail.com>
2023-07-12 19:17:45 -0300

    Add style isPikachu

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -4,10 +4,12 @@ export default function Pokemon() {
     
   const randomNum = Math.floor(Math.random() * 151) + 1;
   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNum}.png`;
+  const isPikachu = randomNum === 25;
+  const goldenBox = {backgroundColor: isPikachu ? "gold" : "#fff"};
     
     return (
-      <div className="Box">
-        <h2 className="pokemonNum">Pokemon #{randomNum}</h2>
+      <div className="Box" style={goldenBox}>
+        <h2 className="pokemonNum">Pokémon #{randomNum}</h2>
         <img src={url} alt="pokemon" />
       </div>
     );
