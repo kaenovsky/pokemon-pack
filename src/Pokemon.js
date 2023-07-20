32d3ab5f0c088dca1da9643928ad0d1685a155e3
kaenovsky <martinkaen@gmail.com>
2023-07-20 20:44:57 -0300

    Add useState to component

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,14 +1,25 @@
 import "./Pokemon.css";
+import { useState } from "react";
 
 export default function Pokemon() {
-    
-  const randomNum = Math.floor(Math.random() * 151) + 1;
-  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNum}.png`;
-  const isPikachu = randomNum === 25;
-  const goldenBox = {backgroundColor: isPikachu ? "gold" : "#fff"};
-    
+
+  const newPokemon = () => {
+    const randomNum = Math.floor(Math.random() * 151) + 1;
+    const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNum}.png`;
+    const isPikachu = randomNum === 25;
+    const goldenBox = {backgroundColor: isPikachu ? "gold" : "#fff"};
+    return [url, goldenBox, randomNum];
+  }
+  
+  const [pokemon, setPokemon] = useState();
+  const [url, goldenBox, randomNum] = newPokemon();
+
+  const reloadPokemon = () => {
+    setPokemon(newPokemon());
+  }
+
     return (
-      <div className="Box" style={goldenBox}>
+      <div className="Box" style={goldenBox} onClick={reloadPokemon}>
         <h2 className="pokemonNum">Pokémon #{randomNum}</h2>
         <img src={url} alt="pokemon" />
       </div>
